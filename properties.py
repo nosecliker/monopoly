9af456910fa4d68ae7e4c4d38eb70453577396ac
pencillead2 <bkhoury@fusionacademy.com>
2025-05-09 11:46:50 -0500

    Here are some properties

---
+++ properties.py
@@ -0,0 +1,20 @@
+ori = ["Oriental Avenue", 6, 6, 100, False, 0, 1]
+
+
+
+states = ["States Avenue", 13, 10, 140, False, 0, 2]
+
+stjp = ["St. James Place", 16, 14, 180, False, 0, 3]
+tenn = ["Tennessee Avenue", 18, 14, 180, False, 0, 3]
+nya = ["New York Avenue", 19, 16, 200, False, 0, 3]
+
+ind = ["Indiana Avenue", 23, 18, 220, False, 0, 4]
+ill = ["Illinois Avenue", 24, 20, 240, False, 0, 4]
+atlantic = ["Atlantic Avenue", 26, 22, 260, False, 0, 5]
+vent = ["Ventnor Avenue", 27, 22, 280, False, 0, 5]
+marvin = ["Marvin Gardens", 29, 24, 280, False, 0, 5]
+pacific = ["Pacific Avenue", 31, 26, 300, False, 0, 6]
+nc = ["North Carolina Avenue", 32, 26, 300, False, 0, 6]
+penn = ["Pennsylvania Avenue", 34, 28, 320, False, 0, 6]
+park = ["Park Place", 37, 35, 350, False, 0, 7]
+board = ["Boardwalk", 39, 50, 400, False, 0, 7]
